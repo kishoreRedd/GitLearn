ce5abf9e9804b97835e10ae26861a6cbaad5eb1e
kishore <kishorereddy.ui@gmail.com>
2019-05-25 11:54:08 +0530

    commmit java project

---
+++ javatest/src/main/java/project/Main.java
@@ -0,0 +1,11 @@
+package project.javatest;
+
+/**
+ *
+ * @author Your name
+ */
+public class Main {
+    public static void main(String... param) {
+        System.out.println("Starting application...");
+    }
+}
